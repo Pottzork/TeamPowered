8a0aed0454f8169e3b4bf5e7ac3fa0985878277e
André Pontes <33063611+Pottzork@users.noreply.github.com>
2019-12-07 17:12:17 +0100

    Testing VisualStudio Github, first commit

---
+++ Team powered/Team powered/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_powered
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
